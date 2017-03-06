508169159e4e38f7909b0bff1ac675700a16fb95
dargaCode <darga.code@gmail.com>
2017-03-06 15:16:53 -0800

    Complete basic functionality

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 
-// CONSTANTS
-
-var TEST = 'Madam, did you refer to the sagas of Bob and Anna, the civic planner and the racecar driver? While paddling a kayak at noon with no rotor, Bob became red, and then redder and redder. Anna said, "Wow Dad, if your level of exposure gets much higher, I might be required to become your reviver!"';
-
 // DOM SELECTORS
 
 var editableDiv = document.querySelector('#editable-div');
+var input = document.querySelector('input');
+
+// EVENT BINDINGS
+
+editableDiv.addEventListener('input', function(event) {
+  highlightAllPalindromes();
+});
 
 // FUNCTIONS
+
+function highlightAllPalindromes() {
+  const childDivs = editableDiv.children;
+
+  for (div of childDivs) {
+    highlightPalindromesInDiv(div);
+  }
+}
+
+function highlightPalindromesInDiv(div) {
+  const divHTML = div.innerHTML;
+
+  // do nothing to blank <br> divs. This prevents the empty space from being stripped out.
+  if (divHTML === '<br>') {
+    return;
+  }
+
+  const divText = div.innerText;
+  const divWords = divText.split(" ");
+
+  for (let i = 0; i < divWords.length; i++) {
+    const word = divWords[i];
+    const palindromic = isPalindromicWord(word);
+
+    if (palindromic) {
+      divWords[i] = `<span class="highlight">${word}</span>`;
+    }
+  }
+
+  const newContent = divWords.join(' ');
+
+  div.innerHTML = newContent;
+}
 
 function isPalindromicWord(word) {
   let frontWord = '';
@@ -20,53 +56,57 @@ function isPalindromicWord(word) {
     var frontChar = word.charAt(frontIndex);
     var backChar = word.charAt(backIndex);
 
-    if (frontChar != ',') {
+    if (isCharValid(frontChar)) {
       frontWord += frontChar.toLowerCase();
     }
 
-    if (backChar != ',') {
+    if (isCharValid(backChar)) {
       backWord += backChar.toLowerCase();
     }
+  }
+
+  if (frontWord.length === 0) {
+    return false;
   }
 
   return frontWord === backWord;
 }
 
-function charsMatch(charA, charB) {
-  const lowerA = charA.toLowerCase();
-  const lowerB = charB.toLowerCase();
-
-  return (lowerA === lowerB);
-}
-
-function isCharLetter(char) {
+function isCharValid(char) {
+  char = char.toLowerCase();
   const charCode = char.charCodeAt(0);
 
-  console.log(charCode);
+  const isDigit = isCharCodeDigit(charCode);
+  const isLetter = isCharCodeLowerCaseLetter(charCode);
+
+  // Numeric characters count as valid palindrome ingredients.
+  const isValid = isDigit || isLetter;
+
+  return isValid;
+}
+
+function isCharCodeLowerCaseLetter(charCode) {
+  const charCodeA = 'a'.charCodeAt(0);
+  const charCodeZ = 'z'.charCodeAt(0);
+
+  const isLowerCaseLetter = charCode >= charCodeA && charCode <= charCodeZ;
+
+  return isLowerCaseLetter;
+}
+
+function isCharCodeDigit(charCode) {
+  const charCodeZero = '0'.charCodeAt(0);
+  const charCodeNine = '9'.charCodeAt(0);
+
+  const isDigit = charCode >= charCodeZero && charCode <= charCodeNine;
+
+  return isDigit;
 }
 
 // MAIN
 
 (function main() {
-  const childDivs = editableDiv.children;
+  highlightAllPalindromes();
 
-  for (div of childDivs) {
-    const divText = div.innerText;
-    const divWords = divText.split(" ");
-
-    for (let i = 0; i < divWords.length; i++) {
-      const word = divWords[i];
-      const palindromic = isPalindromicWord(word);
-
-      if (palindromic) {
-        divWords[i] = `!!${word}!!`;
-      }
-    }
-
-    console.log(divWords.join(' '));
-  }
-
+  editableDiv.focus();
 }())
-
-
-
